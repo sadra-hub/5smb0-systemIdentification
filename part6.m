62d012058619145d02e2e6dece5b339f0bd36444
Sadra <84704927+sadra-hub@users.noreply.github.com>
2025-03-28 05:30:25 +0100

    modified comments

--- part6.m
+++ part6.m
@@ -5,30 +5,32 @@ N = 3000;               % Number of data points
 M = 1.8;                % Maximum value of input signal
 x = 0.69969;            % Bandwidth range
 
-Band = [0 x];          % Frequency range
-Range = [-M, M];       % Range of values for input signal
+Band = [0 x];           % Frequency range
+Range = [-M, M];        % Range of values for input signal
 
 % Generate a random reference signal for the closed-loop system
-r_prbs = idinput(N, 'prbs', Band, Range);  % Use PRBS for reference signal
+r_prbs = idinput(N, 'prbs', Band, Range);
 
-% Get closed-loop data using the modified function
-[u_prbs, y_prbs] = assignment_sys_33(r_prbs, 'closed loop');  % 'closed loop' mode
-data_prbs_close = iddata(y_prbs, u_prbs);  % Create the closed-loop data object
+% Get closed-loop and open-loop data using the assignment_sys_33 function
+[u_prbs, y_prbs] = assignment_sys_33(r_prbs, 'closed loop');
+data_prbs_close = iddata(y_prbs, u_prbs); 
 
 [u_prbs, y_prbs] = assignment_sys_33(r_prbs, 'open loop');
 data_prbs_open = iddata(y_prbs, u_prbs);
 
 % perform system identification for the closed-loop system using the OE model
+% model orders are optimized by AIC value
+
 model_OE_close = oe(data_prbs_close, [5 5 0]);
 model_OE_open = oe(data_prbs_open, [5 4 1]);
 
-% Compare the closed-loop and open-loop models against new data
+% Compare the closed-loop and open-loop models against validation data
 r_prbs = idinput(N, 'prbs', Band, Range);
-[u_prbs, y_prbs] = assignment_sys_33(r_prbs, 'closed loop');  % 'closed loop' mode
-data_prbs__close_val = iddata(y_prbs, u_prbs);  % Create the closed-loop data object
+[u_prbs, y_prbs] = assignment_sys_33(r_prbs, 'closed loop');
+data_prbs__close_val = iddata(y_prbs, u_prbs); 
 
-[u_prbs, y_prbs] = assignment_sys_33(r_prbs, 'open loop');  % 'closed loop' mode
-data_prbs__open_val = iddata(y_prbs, u_prbs);  % Create the closed-loop data object
+[u_prbs, y_prbs] = assignment_sys_33(r_prbs, 'open loop');
+data_prbs__open_val = iddata(y_prbs, u_prbs); 
 
 
 subplot(2, 1, 1);
